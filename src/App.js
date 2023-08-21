70fca225007045fac797c29c793ff0c587167229
Mike <80673026+mssj-11@users.noreply.github.com>
2023-08-20 22:03:23 -0600

    Hero

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Hero from './components/UI/Hero';
+import Exercises from './components/UI/Exercises';
+
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <Exercises />
     </>
   );
 }
---
+++ src/components/UI/Exercises.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import '../../styles/exercises.css';
+import lunges from '../../assets/img/lunges.png';
+import yoga from '../../assets/img/yoga-pose.png';
+import extended from '../../assets/img/extended.png';
+
+
+const Exercises = () => {
+  return (
+    <section>
+        <div className='container exercises_container'>
+            <div className='exercise_top'>
+                <h2 className='section_title'><span className='highlights'>Exercise</span></h2>
+                <p>
+                    The Lorem Ipsum pasta first appeared in a type specimen <br/> book in the 1500s as a placeholder text.
+                </p>
+            </div>
+
+            {/* ====     Exercises List   ==== */}
+            <div className='exercise_wrapper'>
+                <div className='exercise_item'>
+                    <span className='exercise_icon'>
+                        <img src={lunges} alt='lunges' />
+                    </span>
+
+                    <div className='exercise_content'>
+                        <h4>Healthy Life</h4>
+                        <p>
+                            The Lorem Ipsum pasta first appeared in a type specimen <br/> book in the 1500s as a placeholder text.
+                        </p>
+                    </div>
+                </div>
+
+                
+                <div className='exercise_item'>
+                    <span className='exercise_icon'>
+                        <img src={yoga} alt='lunges' />
+                    </span>
+
+                    <div className='exercise_content'>
+                        <h4>Increased Flexibility</h4>
+                        <p>
+                            The Lorem Ipsum pasta first appeared in a type specimen <br/> book in the 1500s as a placeholder text.
+                        </p>
+                    </div>
+                </div>
+
+
+                <div className='exercise_item'>
+                    <span className='exercise_icon'>
+                        <img src={extended} alt='lunges' />
+                    </span>
+
+                    <div className='exercise_content'>
+                        <h4>Reducing Blood Pressur</h4>
+                        <p>
+                            The Lorem Ipsum pasta first appeared in a type specimen <br/> book in the 1500s as a placeholder text.
+                        </p>
+                    </div>
+                </div>
+                
+            </div>
+
+        </div>
+    </section>
+  );
+}
+
+export default Exercises
