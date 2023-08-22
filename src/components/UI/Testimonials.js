2dcca5cb847b2e9d0312ec75cdba67fff7c96c92
Mike <80673026+mssj-11@users.noreply.github.com>
2023-08-22 12:51:50 -0600

    Testimonials

--- src/components/UI/Testimonials.js
+++ src/components/UI/Testimonials.js
@@ -1,40 +1,70 @@
-import React from 'react';
-import '../../styles/testimonials.css';
+import React from "react";
+import "../../styles/testimonials.css";
+import avatar01 from "../../assets/img/avatar01.png";
+import avatar02 from "../../assets/img/avatar02.png";
 
 // Import Swiper React components
+import { EffectCards } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/effect-cards";
-// import required modules
-import { EffectCards } from "swiper";
+
 
 export default function Testimonials() {
   return (
     <>
       <section>
-        <div className='container'>
+        <div className="container sliders">
+            <h2 className="section_title">Testimonials</h2>
+
             <Swiper
                 effect={"cards"}
                 grabCursor={true}
                 modules={[EffectCards]}
                 className="mySwiper"
             >
-                <SwiperSlide>Slide 1</SwiperSlide>
-                <SwiperSlide>Slide 2</SwiperSlide>
-                <SwiperSlide>Slide 3</SwiperSlide>
-                <SwiperSlide>Slide 4</SwiperSlide>
-                <SwiperSlide>Slide 5</SwiperSlide>
-                <SwiperSlide>Slide 6</SwiperSlide>
-                <SwiperSlide>Slide 7</SwiperSlide>
-                <SwiperSlide>Slide 8</SwiperSlide>
-                <SwiperSlide>Slide 9</SwiperSlide>
+                <SwiperSlide>
+                  <div className="slide_item">
+                    <div className="slide_img-01">
+                      <img src={avatar01} alt="avatar01" />
+                    </div>
+                    <h4>Jessica Fernandes</h4>
+                    <p>
+                        The Lorem Ipsum pasta first appeared in a type specimen book in the 1500s as a placeholder text.
+                        The Lorem Ipsum pasta first appeared in a type specimen book in the 1500s as a placeholder text.
+                    </p>
+                  </div>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                  <div className="slide_item">
+                    <div className="slide_img-02">
+                      <img src={avatar02} alt="avatar02" />
+                    </div>
+                    <h4>Jessica Fernandes</h4>
+                    <p>
+                        The Lorem Ipsum pasta first appeared in a type specimen book in the 1500s as a placeholder text.
+                        The Lorem Ipsum pasta first appeared in a type specimen book in the 1500s as a placeholder text.
+                    </p>
+                  </div>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                  <div className="slide_item">
+                    <div className="slide_img-03">
+                      <img src={avatar01} alt="avatar03" />
+                    </div>
+                    <h4>Jessica Fernandes</h4>
+                    <p>
+                        The Lorem Ipsum pasta first appeared in a type specimen book in the 1500s as a placeholder text.
+                        The Lorem Ipsum pasta first appeared in a type specimen book in the 1500s as a placeholder text.
+                    </p>
+                  </div>
+                </SwiperSlide>
             </Swiper>
         </div>
       </section>
     </>
   );
 }
-
-
-export default Testimonials;
