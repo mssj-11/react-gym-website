b2a48120931f2b3552c026b7b73213556b270963
Mike <80673026+mssj-11@users.noreply.github.com>
2023-08-22 22:48:33 -0600

    Footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Exercises from './components/UI/Exercises';
 import Start from './components/UI/Start';
 import Pricing from './components/UI/Pricing';
 import Testimonials from './components/UI/Testimonials';
+import Footer from './components/Footer/Footer';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
       <Start />
       <Pricing />
       <Testimonials />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import '../../styles/footer.css';
+import logo from '../../assets/img/dumble.png';
+
+
+const Footer = () => {
+    const year = new Date().getFullYear();
+
+  return (
+    <footer className='footer'>
+        <div className='container'>
+            <div className='footer_wrapper'>
+                <div className='footer_box'>
+
+                    {/* LOGO */}
+                    <div className='logo'>
+                        <div className='logo_img'>
+                            <img src={logo} alt='' />
+                        </div>
+                        <h2>Fussion<span className='highlights'> Fit</span></h2>
+                    </div>
+                    <p>
+                        The Lorem Ipsum pasta first appeared in a type specimen book in the 1500s as a placeholder text.
+                    </p>
+                </div>
+
+                {/* FOOTER */}
+                <div className='footer_box'>
+                    <h4 className='footer_title'>Company</h4>
+                    <ul className='footer_links'>
+                        <li><a href='#' >Our Program</a></li>
+                        <li><a href='#' >Our Plan</a></li>
+                        <li><a href='#' >Become a member</a></li>
+                    </ul>
+                </div>
+
+                <div className='footer_box'>
+                    <h4 className='footer_title'>Quick Links</h4>
+                    <ul className='footer_links'>
+                        <li><a href='#' >About Us</a></li>
+                        <li><a href='#' >Contact us</a></li>
+                        <li><a href='#' >Support</a></li>
+                    </ul>
+                </div>
+
+                <div className='footer_box'>
+                    <h4 className='footer_title'>Quick Links</h4>
+                    <ul className='footer_links'>
+                        <li><a href='#' >About Us</a></li>
+                        <li><a href='#' >Contact us</a></li>
+                        <li><a href='#' >Support</a></li>
+                    </ul>
+                </div>
+                
+            </div>
+
+            <p className='copyright'>
+                Copyright &copy; {year} developed by <a href='#' className='highlights'> MSS</a>. All rights reserved.
+            </p>
+        </div>
+    </footer>
+  );
+}
+
+export default Footer
